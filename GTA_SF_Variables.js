dbb899533d6ce4babb4c82b54ccd7280fcfe5750
jermaine-rowe <86036895+jermaine-rowe@users.noreply.github.com>
2021-06-17 00:28:12 -0400

    Add files via upload

---
+++ GTA_SF_Variables.js
@@ -0,0 +1,6 @@
+dataLayer = dataLayer || [];
+dataLayer.push({
+'refid' : globalStoreData.refId,
+'funnelAmount' : globalStoreData.funnelAmount,
+'UserEmail' : globalStoreData.email
+ });// JavaScript Document
